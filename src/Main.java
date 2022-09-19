7db1722d9f0a94e0ff4c5de2e0b7a936b8141812
Chriswihu <chris.wihu@gmail.com>
2022-09-19 13:47:08 +0200

    Opretter Switch og Connect

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+import java.sql.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String input;
+
+        while (true)
+        {
+            input = getString("Velkommen, hvad ønsker du at gøre?");
+
+            switch (input)
+            {
+                case "opret":
+
+            }
+        }
+    }
+
+    public static String getString(String s)
+    {
+        System.out.println(s + " : ");
+        Scanner scan = new Scanner(System.in);
+
+        return scan.nextLine();
+    }
+
+    public static void opret()
+    {
+        String sql = "INSERT INTO lånere (";
     }
 }
